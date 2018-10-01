47d695a4b6a420db5884591e135ce02faea7d5d7
pavel <kareglas83@gmail.ru>
2018-10-01 19:38:08 +0300

    Made an introductory task. Returns the list with the positions where the number of calories is exceeded. Also, the condition is the start and end time, which is set by the user.

--- src/main/java/ru/javawebinar/topjava/Main.java
+++ src/main/java/ru/javawebinar/topjava/Main.java
@@ -1,11 +1,16 @@
 package ru.javawebinar.topjava;
 
+import ru.javawebinar.topjava.util.UserMealsUtil;
+
 /**
  * @see <a href="http://topjava.herokuapp.com">Demo</a>
  * @see <a href="https://github.com/JavaOPs/topjava">Initial project</a>
  */
 public class Main {
     public static void main(String[] args) {
+        String[] ar = null;
+       // UserMealsUtil userMealsUtil = new UserMealsUtil();
+        UserMealsUtil.main(ar);
         System.out.format("Hello Topjava Enterprise!");
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,13 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -17,7 +19,10 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 510),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 15,10,0), "Завтрак", 1000),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 15,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 15,20,0), "Ужин", 510)
         );
         getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
@@ -25,7 +30,47 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed>  getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
-        // TODO return filtered list with correctly exceeded field
-        return null;
+
+
+        HashMap<LocalDate, Integer> hm = new HashMap<LocalDate, Integer>();
+        HashMap<LocalDate, Integer> sm = new HashMap<LocalDate, Integer>();
+        List<UserMealWithExceed> userMealWithExceed = new ArrayList<UserMealWithExceed>();
+
+        //base cycle
+
+        Integer am;
+        for (UserMeal i : mealList) {
+
+            am = hm.get(i.getDateTime().toLocalDate());
+            hm.put(i.getDateTime().toLocalDate(), am == null ? i.getCalories() : am + i.getCalories());
+
+        }
+
+        for (Map.Entry<LocalDate, Integer> pair : hm.entrySet()) {
+            if (pair.getValue()>caloriesPerDay)
+                sm.put(pair.getKey(),pair.getValue());
+
+        }
+
+        for (UserMeal i : mealList) {
+            for (Map.Entry<LocalDate,Integer> pair : sm.entrySet()) {
+                if (i.getDateTime().getHour()>startTime.getHour() && i.getDateTime().getHour()<endTime.getHour()) {
+                    if (i.getDateTime().toLocalDate().equals(pair.getKey()))
+                        userMealWithExceed.add(new UserMealWithExceed(i.getDateTime(),i.getDescription(),i.getCalories(),true));
+                    else
+                        userMealWithExceed.add(new UserMealWithExceed(i.getDateTime(),i.getDescription(),i.getCalories(),false));
+                }
+            }
+        }
+
+
+        //Steam API
+        /*
+        mealList.stream().map(i->hm.put(i.getDateTime().toLocalDate(), hm.get(i.getDateTime().toLocalDate()) == null ? i.getCalories() : hm.get(i.getDateTime().toLocalDate()) + i.getCalories())).collect(Collectors.toList());
+        hm.entrySet().stream().filter(x->x.getValue()>caloriesPerDay).map(x->sm.put(x.getKey(),x.getValue())).collect(Collectors.toList());
+
+        mealList.stream().filter(i->i.getDateTime().getHour()>startTime.getHour() && i.getDateTime().getHour()<endTime.getHour()).map(x->sm.entrySet().stream().filter(i->x.getDateTime().toLocalDate().equals(i.getKey())).map(i->userMealWithExceed.add(new UserMealWithExceed(x.getDateTime(),x.getDescription(),x.getCalories(),true))).collect(Collectors.toList())).collect(Collectors.toList());
+        */
+        return userMealWithExceed;
     }
 }
